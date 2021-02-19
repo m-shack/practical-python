5bd85d6a6b31188b4575ed894b2e9986109d5a95
Mark <mark.shack@yahoo.com>
2021-02-19 10:08:32 -0800

    minor update

--- Work/stock.py
+++ Work/stock.py
@@ -8,6 +8,9 @@ class Stock:
         self.name   = name
         self.shares = shares
         self.price  = price
+    
+    def __repr__(self):
+        return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
     def cost(self):
         '''
